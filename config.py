2a5d3aef4ae337ad4306fac383c30acd02ad4358
Kktbots <143538428+Kktbots@users.noreply.github.com>
2023-10-23 15:10:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,14 +6,14 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-API_ID=15254258
-API_HASH="107dc8de4b11afca514ad02f3e1a2efb"
-AUTH_USERS=5756203736
-    BOT_TOKEN ="5928583787:AAF_iQsaJtNJe7nItmbnEEpr9sNXCOB4Fpg"
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+API_ID=23539681
+API_HASH="ae1430184ae21aa81b4b030d1bd75885"
+AUTH_USERS=5007135395
+    BOT_TOKEN ="6548483719:AAFCxCqSDGI_VE1KEjrzwB605uCfm3MAAcE"
+    BOT_TOKEN = os.environ.get("6548483719:AAFCxCqSDGI_VE1KEjrzwB605uCfm3MAAcE")
     # The Telegram API things
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    AUTH_USERS = os.environ.get("OWNER")
+    API_ID = int(os.environ.get("23539681"))
+    API_HASH = os.environ.get("ae1430184ae21aa81b4b030d1bd75885")
+    AUTH_USERS = os.environ.get("5007135395")
 
     
